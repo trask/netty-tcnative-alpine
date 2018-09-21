65499256c49716e1d24d77cd00f3f39aee313f3a
Trask Stalnaker <trask.stalnaker@gmail.com>
2018-09-21 15:52:12 -0700

    Change from building OpenSSL to BoringSSL

--- compile.sh
+++ compile.sh
@@ -3,8 +3,6 @@
 git clone https://github.com/netty/netty-tcnative
 cd netty-tcnative
 git checkout tags/$NETTY_TCNATIVE_TAG
-mvn clean package
+mvn clean package -pl boringssl-static --am
 
-mv openssl-dynamic/target/*.jar /output/
-mv openssl-static/target/*.jar /output/
 mv boringssl-static/target/*.jar /output/
