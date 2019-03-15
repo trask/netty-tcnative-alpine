e0281ba4bb9384f6d3d4d6d9ce50f41312fbfe24
Trask Stalnaker <trask.stalnaker@gmail.com>
2019-03-15 12:36:57 -0700

    Build javadoc jar

--- compile.sh
+++ compile.sh
@@ -3,6 +3,6 @@
 git clone https://github.com/netty/netty-tcnative
 cd netty-tcnative
 git checkout tags/$NETTY_TCNATIVE_TAG
-mvn clean package -pl boringssl-static --am
+mvn clean package javadoc:jar -pl boringssl-static --am
 
 mv boringssl-static/target/*.jar /output/
